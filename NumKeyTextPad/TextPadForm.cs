45f675685a156d8c55c07e2d4ccd275e2d34434a
Sam Towers <stowers@cardinalnewman.ac.uk>
2017-04-01 18:30:23 +0100

    Add global key listening (on numkey5) to toggle view and NotifyIcon.

--- NumKeyTextPad/TextPadForm.cs
+++ NumKeyTextPad/TextPadForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Gma.UserActivityMonitor;
 
 namespace NumKeyTextPad
 {
@@ -23,7 +24,7 @@ namespace NumKeyTextPad
 			// Initialize the TextPadForms component properties (declared in the designer)
 			this.components = new System.ComponentModel.Container();
 			SetupForm();
-			//ListenForKeyInput();
+			ListenForKeyInput();
 		}
 		
 		private void SetupForm()
@@ -33,8 +34,8 @@ namespace NumKeyTextPad
 			this.Opacity = 0.7;
 			this.BackColor = Color.Yellow;
 			this.ShowInTaskbar = false;
-			this.SetMinimized();
-			
+			this.SetWindowState(FormWindowState.Minimized);
+
 			/** Setup TextPad **/
 			textPad = new TextPad();
 			// Update the textPad size with the form size.
@@ -46,12 +47,7 @@ namespace NumKeyTextPad
 			/** Setup NotifyIcon **/
 			setupNotifyIcon();
 		}
-
-		private void SetMinimized(bool minimized = true)
-		{
-			this.WindowState = minimized ? FormWindowState.Minimized : FormWindowState.Normal;
-		}
-
+		
 		private void setupNotifyIcon()
 		{
 			/** Presentation of the Icon **/
@@ -62,17 +58,73 @@ namespace NumKeyTextPad
 
 			/** Context Menu **/
 			notifyIcon.ContextMenu = makeNotifyIconContextMenu();
+
+			/** Double Click to show form **/
+			notifyIcon.DoubleClick += delegate (object sender, EventArgs args)
+			{
+				this.SetWindowState(FormWindowState.Maximized);
+			};
 		}
 
 		private ContextMenu makeNotifyIconContextMenu()
 		{
 			ContextMenu cm = new ContextMenu();
+
+			/** Show Button **/
+			MenuItem showMenuItem = new MenuItem("Show");
+			showMenuItem.Click += delegate (object sender, EventArgs args) {
+				this.SetWindowState(FormWindowState.Maximized);
+			};
+			cm.MenuItems.Add(showMenuItem);
+
+			/** Hide Button **/
+			MenuItem hideMenuItem = new MenuItem("Hide");
+			hideMenuItem.Click += delegate (object sender, EventArgs args) {
+				this.SetWindowState(FormWindowState.Minimized);
+			};
+			cm.MenuItems.Add(hideMenuItem);
+			
+			/** Exit Button **/
 			MenuItem exitMenuItem = new MenuItem("Exit");
 			exitMenuItem.Click += delegate (object sender, EventArgs args) {
 				Application.Exit();
 			};
 			cm.MenuItems.Add(exitMenuItem);
+
 			return cm;
+		}
+		
+		/**
+		 * Listen for the Numpad keyboard events, which will dicate the visibly and positioning 
+		 * of the textpad.
+		 */
+		private void ListenForKeyInput()
+		{
+			HookManager.KeyDown += HandleKeyPress;
+		}
+
+		private void HandleKeyPress(object sender, KeyEventArgs args)
+		{
+			if (args.KeyCode == Keys.NumPad5)
+			{
+				args.Handled = true;
+				SetWindowState(); 
+			}
+		}
+
+		/**
+		 * Set the form window state or toggle between Maximized and Minimized.
+		 */
+		private void SetWindowState(FormWindowState? forceState = null)
+		{
+			if (forceState.HasValue)
+			{
+				this.WindowState = forceState.Value;
+			}
+			else
+			{
+				this.WindowState = this.WindowState == FormWindowState.Maximized ? FormWindowState.Minimized : FormWindowState.Maximized;
+			}
 		}
 	}
 }
